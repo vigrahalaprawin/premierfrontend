5e8f0fbd63dd90a70df315b6058c45a64ecaa3e2
Praveen Kumar V <66878643+vigrahalaprawin@users.noreply.github.com>
2024-02-05 19:29:25 +0530

    updated for addPremier without teamId & ListUpdation with Delete option

--- src/components/AddPremierTeam.jsx
+++ src/components/AddPremierTeam.jsx
@@ -1,23 +1,21 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import ListPremierTeams from "./ListPremierTeams";
 
 class AddPremierTeam extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      teamId: "",
       teamName: "",
       teamCoach: "",
       teamStadium: "",
     };
     this.updateFormData = this.updateFormData.bind(this);
     this.premTeamSubmit = this.premTeamSubmit.bind(this);
-    this.formref = React.createRef();
   }
 
   premTeamSubmit(event) {
     const data = {
-      teamId: this.state.teamId,
       teamName: this.state.teamName,
       teamCoach: this.state.teamCoach,
       teamStadium: this.state.teamStadium,
@@ -30,8 +28,8 @@ class AddPremierTeam extends Component {
       body: JSON.stringify(data), // Include the request body as needed
     })
       .then((response) => {
-        // Handle the response
-        console.log("success");
+        console.log("we are in success score");
+        return <ListPremierTeams />;
       })
       .catch((error) => {
         console.log(error);
@@ -49,17 +47,6 @@ class AddPremierTeam extends Component {
       <div>
         <h1>Add Premier League Team</h1>
         <form name="premadd" onSubmit={this.premTeamSubmit}>
-          <div>
-            <label>
-              Enter Club ID
-              <input
-                name="teamId"
-                onChange={this.updateFormData}
-                value={this.state.teamId}
-                type="number"
-              />
-            </label>
-          </div>
           <div>
             <label>
               Enter Club Name
@@ -93,9 +80,11 @@ class AddPremierTeam extends Component {
               />
             </label>
           </div>
-          <button type="button" className="m5" onClick={this.premTeamSubmit}>
-            Submit
-          </button>
+          <Link to="/">
+            <button type="button" className="m5" onClick={this.premTeamSubmit}>
+              Submit
+            </button>
+          </Link>
         </form>
         <Link to="/">
           <button className="m5">Back to home Page </button>
---
+++ src/components/ListPremierTeams.jsx
@@ -0,0 +1,79 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import PremierService from "../service/PremierService";
+
+class ListPremierTeams extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      premTeams: [],
+    };
+  }
+
+  componentDidMount() {
+    PremierService.getPremierTeams().then((response) => {
+      this.setState({ premTeams: response.data });
+    });
+  }
+
+  deletePremTeam(event) {
+    const premId = event.target.id;
+    fetch(`http://localhost:8080/api/deleteTeam/${premId}`, {
+      //Sending the parameter in url
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json", // Adjust the Content-Type as needed
+      },
+    })
+      .then((response) => {
+        // Handle the response
+        console.log("success");
+      })
+      .catch((error) => {
+        console.log(error);
+        // Handle any errors
+      });
+
+    window.location.reload();
+  }
+
+  render() {
+    return (
+      <div>
+        <h2 className="text-center">Premier League Teams</h2>
+        <Link to="/addTeam">
+          <button className="btn btn-primary m5 addTeambasic">Add Team</button>
+        </Link>
+
+        <div className="row">
+          <table className="table  table-striped table-bordered">
+            <thead>
+              <tr>
+                <th>Club Name</th>
+                <th>Club Coach</th>
+                <th>Club Stadium</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.premTeams.map((premTeam) => (
+                <tr key={premTeam.id}>
+                  <td>{premTeam.teamName}</td>
+                  <td>{premTeam.teamCoach}</td>
+                  <td>{premTeam.teamStadium}</td>
+                  <td>
+                    <button id={premTeam.id} onClick={this.deletePremTeam}>
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ListPremierTeams;
